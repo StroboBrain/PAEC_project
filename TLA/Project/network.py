947c43e6b1e59ffbaf59d8a7300cce0d34514bcb
strobobrain <parkschaden@gmail.com>
2026-05-15 07:44:38 +0200

    set up testsuit

---
+++ TLA/Project/network.py
@@ -0,0 +1,39 @@
+"""
+This module contains the Network class, which represents a network, could be repalaced by a real network
+"""
+
+
+
+class Network_Handler:
+    def __init__(self, network_config):
+        self.network_config = network_config
+
+    
+    
+    def setup_network(self):
+        # Simulate setting up the network based on the configuration
+        pass
+
+"""
+Simple network simulator
+"""
+
+class Network_Simulator:
+    def __init__(self):
+        self.user_list = []
+        self.message_queue = []
+
+
+    def add_user(self, name):
+        # Simulate adding a user to the network
+        pass
+
+    def send_message(self, sender, receiver, message):
+        # Simulate sending a message from sender to receiver
+        pass
+
+    def receive_message(self, receiver):
+        # Simulate receiving a message for the receiver
+        pass
+
+    
